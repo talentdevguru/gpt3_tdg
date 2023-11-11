a660a5d45a1bd1c03a4eace745edb832c1516e20
talentdevguru <talent.dev.guru@gmail.com>
2023-11-11 08:31:56 -0800

    File && Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Header,
+  WhatGPT3,
+  Features,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
